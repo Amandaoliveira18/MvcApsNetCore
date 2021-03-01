23d328248496353c7c8d2a10963e99790473c6ce
Amandaoliveira18 <amanda-santos1998@hotmail.com>
2021-03-01 09:06:41 -0300

    Simple Create form

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
 using SalesWebMvc.Services;
 
 namespace SalesWebMvc.Controllers
@@ -21,7 +22,17 @@ namespace SalesWebMvc.Controllers
             return View(list); //ao passar como argumento o list, é gerado um action Result
 
         }
-
-
+        public IActionResult Create()
+        {
+            return View();
+        }
+        
+        [HttpPost] //Para indicar que é uma requisição post, e não GET.
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller) //Para fazer a requisição só passar como parametro o objeto
+        {
+            _sellerService.Insert(seller);//Fiz a inserção
+            return RedirectToAction(nameof(Index)); //Para redirecionar a minha requisição para o inddex 
+        }
     }
 }
--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -20,5 +20,12 @@ namespace SalesWebMvc.Services
         {
             return _context.Seller.ToList();
         }
+
+        public void Insert(Seller obj)
+        {
+            _context.Add(obj);//faz a adição
+            _context.SaveChanges();//salva o insert, ou a alteração no BD
+
+        }
     }
 }
